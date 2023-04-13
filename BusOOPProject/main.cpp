d6172d03e6f1283277c6ce2e55976c98e27ca11f
whosfernout <artyomlordik@gmail.com>
2023-04-13 17:49:25 +0300

    Add project files.

---
+++ BusOOPProject/main.cpp
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "bus.h"
+
+int main() {
+	Bus bus1;
+	bus1.brand = "MAZ";
+	bus1.number = "1234PM7";
+	bus1.price = 14000;
+	bus1.capasity = 10
+
+	return 0;
+}
+
+
+// Bus 1: MAZ, 1234PM7, $ 14000, 10	-
+// Bus 2: PAZ, 2345PM7, $ 23500, 15	- 
+// Bus 3: Mercedes Benz, 3456PM7, $ 21650, 17	- 
+// Bus 4: Scania, 4567PM7, $ 45000, 30	- 
+// Bus 5: Neoplan, 5678PM7, $ 113000, 87	- 
+
